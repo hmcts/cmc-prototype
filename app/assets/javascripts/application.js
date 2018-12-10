fe77393d0484314b468254a7399ccf328c7ce31b
Millie Chan <millie.chan@hmcts.net>
2018-12-10 21:39:24 +0000

    New dashboard - documents tab exploration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -956,50 +956,75 @@ function tableRow (n) {
 }
 
 /* CASE FILE */
-var Casefile = function (container) {
-  this.container = container
-  this.items = container.find('.jui-cf__files')
-  this.comments = container.find('.jui-cf__document-comments')
-  this.itemsButton = container.find('.jui-cf__toolbar-button--items')
-  this.commentsButton = container.find('.jui-cf__toolbar-button--comments')
-  this.itemsButton.on('click', $.proxy(this, 'onButtonItemsClick'))
-  this.commentsButton.on('click', $.proxy(this, 'onButtonCommentsClick'))
-  this.showItems()
-  this.hideComments()
-}
+// var Casefile = function (container) {
+//   this.container = container
+//   this.items = container.find('.jui-cf__files')
+//   this.comments = container.find('.jui-cf__document-comments')
+//   this.itemsButton = container.find('.jui-cf__toolbar-button--items')
+//   this.commentsButton = container.find('.jui-cf__toolbar-button--comments')
+//   this.itemsButton.on('click', $.proxy(this, 'onButtonItemsClick'))
+//   this.commentsButton.on('click', $.proxy(this, 'onButtonCommentsClick'))
+//   this.showItems()
+//   this.hideComments()
+// }
+//
+// Casefile.prototype.hideComments = function () {
+//   this.container.removeClass('jui-cf--show-comments')
+//   this.comments.hide()
+//   this.commentsButton.attr('aria-pressed', 'false')
+// }
+//
+// Casefile.prototype.hideItems = function () {
+//   this.items.hide()
+//   this.itemsButton.attr('aria-pressed', 'false')
+// }
+//
+// Casefile.prototype.showComments = function () {
+//   this.container.addClass('jui-cf--show-comments')
+//   this.comments.show()
+//   this.commentsButton.attr('aria-pressed', 'true')
+// }
+//
+// Casefile.prototype.showItems = function () {
+//   this.items.show()
+//   this.itemsButton.attr('aria-pressed', 'true')
+// }
+//
+// Casefile.prototype.onButtonItemsClick = function (e) {
+//   this.showItems()
+//   this.hideComments()
+// }
+//
+// Casefile.prototype.onButtonCommentsClick = function (e) {
+//   this.showComments()
+//   this.hideItems()
+// }
 
-Casefile.prototype.hideComments = function () {
-  this.container.removeClass('jui-cf--show-comments')
-  this.comments.hide()
-  this.commentsButton.attr('aria-pressed', 'false')
-}
+/* CASE FILE 2*/
+var Casefile = function(container) {
+  this.container = container;
+  this.items = container.find('.jui-cf__files');
+  this.comments = container.find('.jui-cf__document-comments');
+  this.itemsButton = container.find('.jui-cf__toolbar-button--items');
+  this.commentsButton = container.find('.jui-cf__toolbar-button--comments');
+  this.itemsButton.on('click', $.proxy(this, 'onButtonItemsClick'));
+  this.commentsButton.on('click', $.proxy(this, 'onButtonCommentsClick'));
+  this.showItems();
+  this.hideComments();
+};
 
-Casefile.prototype.hideItems = function () {
-  this.items.hide()
-  this.itemsButton.attr('aria-pressed', 'false')
-}
 
-Casefile.prototype.showComments = function () {
-  this.container.addClass('jui-cf--show-comments')
-  this.comments.show()
-  this.commentsButton.attr('aria-pressed', 'true')
-}
+Casefile.prototype.hideComments = function() {
+  this.container.removeClass('jui-cf--show-comments');
+  this.comments.hide();
+  this.commentsButton.attr('aria-pressed', 'false');
+};
 
-Casefile.prototype.showItems = function () {
-  this.items.show()
-  this.itemsButton.attr('aria-pressed', 'true')
-}
 
-Casefile.prototype.onButtonItemsClick = function (e) {
-  this.showItems()
-  this.hideComments()
-}
-
-Casefile.prototype.onButtonCommentsClick = function (e) {
-  this.showComments()
-  this.hideItems()
-}
-
+Casefile.prototype.hideItems = function() {
+  this.items.hide();
+  this.itemsButton.attr('aria-pressed', 'false');
+};
 
 // //Select entire table row
 // $(".table-clickable tbody tr").click(function (e) {
