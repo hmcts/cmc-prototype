9960d15ba7b385c7ddb9d32f54469261fcc7e27e
Millie Chan <millie.chan@hmcts.net>
2018-12-10 12:43:04 +0000

    ROC-4826 minor tweaks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1017,3 +1017,46 @@ $(document).ready(function () {
   tabs();
 });
 
+function daysPass (page, waitTime, days) {
+  var theWaitTime = waitTime || 3000
+  setTimeout(function () {
+    $('#triggerDaysPassing').addClass('active')
+  }, theWaitTime)
+  $('#triggerDaysPassing').on('click', function () {
+    $('#daysPassing').addClass('active')
+    $('#triggerDaysPassing').removeClass('active')
+    for (var i = 0; i <= days; ++i) {
+      setTimeout((function (x) {
+        return function () {
+          $('#days').html(x)
+        }
+      })(i), i * 1000)
+    }
+    setTimeout(function () { document.location.href = page }, (days * 1000))
+  })
+}
+
+if ($('#daysPassing').length === 1) {
+  var url = ($('#daysPassing').attr('data-dayspassurl'))
+  var timeToPrompt = ($('#daysPassing').attr('data-dayspasstimetoprompt')) || 5000
+  var days = ($('#daysPassing').attr('data-dayspassdays')) || 5
+  daysPass(url, timeToPrompt, days)
+}
+
+if ($('#phoneBackground #phone-mockup').length === 1) {
+  $('#phone-mockup').on('click', function () {
+    $('#phoneBackground').toggleClass('active')
+    $(this).toggleClass('active')
+  })
+}
+
+if ($('#phoneAccessCode #phone-mockup').length === 1) {
+  setTimeout(function () {
+    $('#phone-mockup').toggleClass('active')
+  }, (3000))
+}
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
+})
+
