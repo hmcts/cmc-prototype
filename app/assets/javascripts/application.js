bc4bc7039693372959e050ea839554453f949adc
Millie Chan <millie.chan@hmcts.net>
2018-12-11 12:15:41 +0000

    New dashboard - documents exploration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1196,3 +1196,12 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
+
+/* SIDE TABS */
+$('ul.tab__nav li a').click(function(){
+  var target = "#" + $(this).data("target");
+  $("ul.tab__nav li a").removeClass('active');
+  $(this).addClass('active');
+  $('.tab__content').not(target).addClass('js-hidden');
+  $(target).removeClass('js-hidden');
+});
